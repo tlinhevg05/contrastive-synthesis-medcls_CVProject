85696d85a9c7b1744da21a4f09d7cae0317e81a8
vuthuylinh <tlinh.evg05@gmail.com>
2026-06-06 19:54:16 +0700

    refactor

--- scripts/eval_vit.sh
+++
@@ -1,5 +0,0 @@
-python -m scripts.evaluate_classification_vit \
-    --config configs/classification/finetune_dino.yaml \
-    --checkpoint_path /path/to/your/best_finetune_model.pth \
-    --data_path /path/to/your/data \
-    --output_dir results/test_evaluation 
